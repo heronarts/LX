d9bf9ee9b06c7f4f7016e3cf572b339aa5fb388d
Mark Slee <mcslee@gmail.com>
2018-08-15 13:51:58 -0700

    fix initial blend registration

--- src/heronarts/lx/LX.java
+++ src/heronarts/lx/LX.java
@@ -39,9 +39,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -209,49 +207,23 @@ public class LX {
   private final List<Class<? extends LXEffect>> registeredEffects =
     new ArrayList<Class<? extends LXEffect>>();
 
-  private static final Class<? extends LXBlend>[] DEFAULT_CHANNEL_BLENDS;
-  private static final Class<? extends LXBlend>[] DEFAULT_TRANSITION_BLENDS;
-  private static final Class<? extends LXBlend>[] DEFAULT_CROSSFADER_BLENDS;
-
-  static {
-    @SuppressWarnings("unchecked")
-    Class<? extends LXBlend>[] type = (Class<? extends LXBlend>[]) Array.newInstance(LXBlend.class, 0);
-
-    List<Class<? extends LXBlend>> channelBlends = new ArrayList<Class<? extends LXBlend>>();
-    channelBlends.add(AddBlend.class);
-    channelBlends.add(MultiplyBlend.class);
-    channelBlends.add(SubtractBlend.class);
-    channelBlends.add(DifferenceBlend.class);
-    channelBlends.add(NormalBlend.class);
-    DEFAULT_CHANNEL_BLENDS = channelBlends.toArray(type);
-
-    List<Class<? extends LXBlend>> transitionBlends = new ArrayList<Class<? extends LXBlend>>();
-    transitionBlends.add(DissolveBlend.class);
-    transitionBlends.add(AddBlend.class);
-    transitionBlends.add(MultiplyBlend.class);
-    transitionBlends.add(LightestBlend.class);
-    transitionBlends.add(DarkestBlend.class);
-    transitionBlends.add(DifferenceBlend.class);
-    DEFAULT_TRANSITION_BLENDS = DEFAULT_CROSSFADER_BLENDS = transitionBlends.toArray(type);
-  }
-
   /**
    * The list of globally registered channel blend classes
    */
   private final List<Class<? extends LXBlend>> registeredChannelBlends =
-    new ArrayList<Class<? extends LXBlend>>(Arrays.asList(DEFAULT_CHANNEL_BLENDS));
+    new ArrayList<Class<? extends LXBlend>>();
 
   /**
    * The list of globally registered transition blend classes
    */
   private final List<Class<? extends LXBlend>> registeredTransitionBlends =
-    new ArrayList<Class<? extends LXBlend>>(Arrays.asList(DEFAULT_TRANSITION_BLENDS));
+    new ArrayList<Class<? extends LXBlend>>();
 
   /**
    * The list of globally registered crossfader blend classes
    */
   private final List<Class<? extends LXBlend>> registeredCrossfaderBlends =
-    new ArrayList<Class<? extends LXBlend>>(Arrays.asList(DEFAULT_CROSSFADER_BLENDS));
+    new ArrayList<Class<? extends LXBlend>>();
 
   /**
    * Creates an LX instance with no nodes.
@@ -316,6 +288,9 @@ public class LX {
     this.palette = new LXPalette(this);
     LX.initTimer.log("Palette");
 
+    // Default blends
+    registerDefaultBlends();
+
     // Construct the engine
     this.engine = new LXEngine(this);
     LX.initTimer.log("Engine");
@@ -330,6 +305,23 @@ public class LX {
     // Add a default channel
     this.engine.addChannel(new LXPattern[] { new IteratorPattern(this) }).fader.setValue(1);
     LX.initTimer.log("Default Channel");
+  }
+
+  private void registerDefaultBlends() {
+    this.registeredChannelBlends.add(AddBlend.class);
+    this.registeredChannelBlends.add(MultiplyBlend.class);
+    this.registeredChannelBlends.add(SubtractBlend.class);
+    this.registeredChannelBlends.add(DifferenceBlend.class);
+    this.registeredChannelBlends.add(NormalBlend.class);
+
+    this.registeredTransitionBlends.add(DissolveBlend.class);
+    this.registeredTransitionBlends.add(AddBlend.class);
+    this.registeredTransitionBlends.add(MultiplyBlend.class);
+    this.registeredTransitionBlends.add(LightestBlend.class);
+    this.registeredTransitionBlends.add(DarkestBlend.class);
+    this.registeredTransitionBlends.add(DifferenceBlend.class);
+
+    this.registeredCrossfaderBlends.addAll(this.registeredTransitionBlends);
   }
 
   public LX addListener(Listener listener) {
