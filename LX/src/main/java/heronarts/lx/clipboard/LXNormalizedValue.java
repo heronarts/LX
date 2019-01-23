fa5173232e3428a5aae1dbe065741f102784cadb
Mark Slee <mcslee@gmail.com>
2019-01-22 17:26:40 -0800

    Add normalized value container for clipboard

---
+++ LX/src/main/java/heronarts/lx/clipboard/LXNormalizedValue.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2019- Mark C. Slee, Heron Arts LLC
+ *
+ * This file is part of the LX Studio software library. By using
+ * LX, you agree to the terms of the LX Studio Software License
+ * and Distribution Agreement, available at: http://lx.studio/license
+ *
+ * Please note that the LX license is not open-source. The license
+ * allows for free, non-commercial use.
+ *
+ * HERON ARTS MAKES NO WARRANTY, EXPRESS, IMPLIED, STATUTORY, OR
+ * OTHERWISE, AND SPECIFICALLY DISCLAIMS ANY WARRANTY OF
+ * MERCHANTABILITY, NON-INFRINGEMENT, OR FITNESS FOR A PARTICULAR
+ * PURPOSE, WITH RESPECT TO THE SOFTWARE.
+ *
+ * @author Mark C. Slee <mark@heronarts.com>
+ */
+
+package heronarts.lx.clipboard;
+
+import heronarts.lx.parameter.CompoundParameter;
+import heronarts.lx.parameter.LXNormalizedParameter;
+
+public class LXNormalizedValue implements LXClipboardItem {
+
+  public final double value;
+
+  public LXNormalizedValue(CompoundParameter p) {
+    this(p.getBaseNormalized());
+  }
+
+  public LXNormalizedValue(LXNormalizedParameter p) {
+    this(p.getNormalized());
+  }
+
+  public LXNormalizedValue(double value) {
+    this.value = value;
+  }
+
+  public double getValue() {
+    return this.value;
+  }
+
+  @Override
+  public LXClipboardItem duplicate() {
+    return new LXNormalizedValue(this.value);
+  }
+}
