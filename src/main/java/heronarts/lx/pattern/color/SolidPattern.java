47b86b80b2fa2e70c5d30c3daeb575daa08126dc
Mark Slee <mcslee@gmail.com>
2020-05-27 20:27:31 -0700

    Color mode options for solid pattern

--- src/main/java/heronarts/lx/pattern/color/SolidPattern.java
+++ src/main/java/heronarts/lx/pattern/color/SolidPattern.java
@@ -22,14 +22,42 @@ import heronarts.lx.LXCategory;
 import heronarts.lx.LX;
 import heronarts.lx.color.ColorParameter;
 import heronarts.lx.color.LXColor;
+import heronarts.lx.color.LXDynamicColor;
+import heronarts.lx.color.LXSwatch;
+import heronarts.lx.parameter.DiscreteParameter;
+import heronarts.lx.parameter.EnumParameter;
 import heronarts.lx.pattern.LXPattern;
 
 @LXCategory(LXCategory.COLOR)
 public class SolidPattern extends LXPattern {
 
+  public enum ColorMode {
+    FIXED("Fixed"),
+    PALETTE("Palette");
+
+    public final String label;
+
+    private ColorMode(String label) {
+      this.label = label;
+    }
+
+    @Override
+    public String toString() {
+      return this.label;
+    }
+  }
+
+  public final EnumParameter<ColorMode> colorMode =
+    new EnumParameter<ColorMode>("Color Mode", ColorMode.FIXED)
+    .setDescription("Which source the gradient selects colors from");
+
   public final ColorParameter color =
     new ColorParameter("Color")
     .setDescription("Color of the pattern");
+
+  public final DiscreteParameter paletteIndex =
+    new DiscreteParameter("Index", 1, LXSwatch.MAX_COLORS + 1)
+    .setDescription("Which index at the palette to start from");
 
   public SolidPattern(LX lx) {
     this(lx, LXColor.RED);
@@ -39,15 +67,29 @@ public class SolidPattern extends LXPattern {
     super(lx);
     this.color.setColor(color);
     addParameter("color", this.color);
+    addParameter("colorMode", this.colorMode);
+    addParameter("paletteIndex", this.paletteIndex);
+  }
+
+  public LXDynamicColor getPaletteColor() {
+    return this.lx.engine.palette.getSwatchColor(this.paletteIndex.getValuei() - 1);
   }
 
   @Override
   public void run(double deltaMs) {
-    // There may be modulators applied to the h/s/b values!
-    setColors(LXColor.hsb(
-      this.color.hue.getValue(),
-      this.color.saturation.getValue(),
-      this.color.brightness.getValue()
-    ));
+    switch (this.colorMode.getEnum()) {
+    case PALETTE:
+      setColors(getPaletteColor().getColor());
+      break;
+    default:
+    case FIXED:
+      // There may be modulators applied to the h/s/b values!
+      setColors(LXColor.hsb(
+        this.color.hue.getValue(),
+        this.color.saturation.getValue(),
+        this.color.brightness.getValue()
+      ));
+      break;
+    }
   }
 }
