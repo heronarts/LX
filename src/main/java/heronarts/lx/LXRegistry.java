cee4d55c3f2352ed2f6f244eb237e051f3c1c951
Mark Slee <mcslee@gmail.com>
2020-06-10 11:22:35 -0700

    Adapt sparkle pattern into an effect also

--- src/main/java/heronarts/lx/LXRegistry.java
+++ src/main/java/heronarts/lx/LXRegistry.java
@@ -122,6 +122,7 @@ public class LXRegistry implements LXSerializable {
     DEFAULT_EFFECTS.add(heronarts.lx.effect.DynamicsEffect.class);
     DEFAULT_EFFECTS.add(heronarts.lx.effect.InvertEffect.class);
     DEFAULT_EFFECTS.add(heronarts.lx.effect.HueSaturationEffect.class);
+    DEFAULT_EFFECTS.add(heronarts.lx.effect.SparkleEffect.class);
     DEFAULT_EFFECTS.add(heronarts.lx.effect.StrobeEffect.class);
   };
 
