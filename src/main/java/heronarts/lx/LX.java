09dc70988132e1a9ec1ed7961cfdf78861a6c71f
Mark Slee <mcslee@gmail.com>
2022-06-02 15:22:16 -0700

    Bump to version 0.4.2-SNAPSHOT

--- src/main/java/heronarts/lx/LX.java
+++ src/main/java/heronarts/lx/LX.java
@@ -67,7 +67,7 @@ import com.google.gson.stream.JsonWriter;
  */
 public class LX {
 
-  public static final String VERSION = "0.4.1";
+  public static final String VERSION = "0.4.2-SNAPSHOT";
 
   public static class InstantiationException extends Exception {
 
