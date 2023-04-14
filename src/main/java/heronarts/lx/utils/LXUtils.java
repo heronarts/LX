5adfdc6f750263f13ccc9fa0342304291f071754
Mark Slee <mcslee@gmail.com>
2023-04-14 09:11:06 +0100

    Add 3d distance utility

--- src/main/java/heronarts/lx/utils/LXUtils.java
+++ src/main/java/heronarts/lx/utils/LXUtils.java
@@ -177,6 +177,10 @@ public class LXUtils {
     return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
   }
 
+  public static double distance(double x1, double y1, double z1, double x2, double y2, double z2) {
+    return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+  }
+
   public static double lerp(double v1, double v2, double amt) {
     return v1 + (v2 - v1) * amt;
   }
