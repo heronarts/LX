a82e87869bc2dfe99a8d385bee374c2332aada8b
Mark Slee <mcslee@gmail.com>
2023-11-22 13:12:49 +0000

    Add a color masking effect for simple blend

--- src/main/java/heronarts/lx/LXRegistry.java
+++ src/main/java/heronarts/lx/LXRegistry.java
@@ -142,6 +142,7 @@ public class LXRegistry implements LXSerializable {
     DEFAULT_EFFECTS.add(heronarts.lx.effect.audio.SoundObjectEffect.class);
     DEFAULT_EFFECTS.add(heronarts.lx.effect.BlurEffect.class);
     DEFAULT_EFFECTS.add(heronarts.lx.effect.color.ColorizeEffect.class);
+    DEFAULT_EFFECTS.add(heronarts.lx.effect.color.ColorMaskEffect.class);
     DEFAULT_EFFECTS.add(heronarts.lx.effect.DynamicsEffect.class);
     DEFAULT_EFFECTS.add(heronarts.lx.effect.InvertEffect.class);
     DEFAULT_EFFECTS.add(heronarts.lx.effect.HueSaturationEffect.class);
