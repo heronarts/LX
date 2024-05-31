07150577f992cc82bdda48170dc61fa58ad909c1
Mark Slee <mcslee@gmail.com>
2024-05-31 14:29:30 +0100

    Add utility inrange method for checking min + max bounds in one call

--- src/main/java/heronarts/lx/utils/LXUtils.java
+++ src/main/java/heronarts/lx/utils/LXUtils.java
@@ -194,6 +194,42 @@ public class LXUtils {
   }
 
   /**
+   * Returns true if value is between [min, max] inclusive
+   *
+   * @param val Value
+   * @param min Min value
+   * @param max Max value
+   * @return True if contained in range
+   */
+  public static boolean inRange(int val, int min, int max) {
+    return (val >= min) && (val <= max);
+  }
+
+  /**
+   * Returns true if value is between [min, max] inclusive
+   *
+   * @param val Value
+   * @param min Min value
+   * @param max Max value
+   * @return True if contained in range
+   */
+  public static boolean inRange(float val, float min, float max) {
+    return (val >= min) && (val <= max);
+  }
+
+  /**
+   * Returns true if value is between [min, max] inclusive
+   *
+   * @param val Value
+   * @param min Min value
+   * @param max Max value
+   * @return True if contained in range
+   */
+  public static boolean inRange(double val, double min, double max) {
+    return (val >= min) && (val <= max);
+  }
+
+  /**
    * Inverse linear interpolation, normalizes a value relative to bounds
    *
    * @param amt Interpolated value
