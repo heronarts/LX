6c92b16a9989552bc16186f672de6fb2a319297c
Mark Slee <mcslee@gmail.com>
2024-11-27 15:22:33 +0000

    Docblock license and author for sysex midi stuff

--- src/main/java/heronarts/lx/midi/LXSysexMessage.java
+++ src/main/java/heronarts/lx/midi/LXSysexMessage.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2024- Justin K. Belcher, Heron Arts LLC
+ *
+ * This file is part of the LX Studio software library. By using
+ * LX, you agree to the terms of the LX Studio Software License
+ * and Distribution Agreement, available at: http://lx.studio/license
+ *
+ * Please note that the LX license is not open-source. The license
+ * allows for free, non-commercial use.
+ *
+ * HERON ARTS MAKES NO WARRANTY, EXPRESS, IMPLIED, STATUTORY, OR
+ * OTHERWISE, AND SPECIFICALLY DISCLAIMS ANY WARRANTY OF
+ * MERCHANTABILITY, NON-INFRINGEMENT, OR FITNESS FOR A PARTICULAR
+ * PURPOSE, WITH RESPECT TO THE SOFTWARE.
+ *
+ * @author Justin K. Belcher <jkbelcher@gmail.com>
+ */
 package heronarts.lx.midi;
 
 import javax.sound.midi.ShortMessage;
