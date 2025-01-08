17a5e5645fb950be0bc4ac50d5b8075ca5625774
Mark Slee <mcslee@gmail.com>
2025-01-08 12:17:51 +0000

    Use a managed pending instance array for more efficient quantized parameter dispatching

--- src/main/java/heronarts/lx/parameter/QuantizedTriggerParameter.java
+++ src/main/java/heronarts/lx/parameter/QuantizedTriggerParameter.java
@@ -19,11 +19,14 @@
 package heronarts.lx.parameter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import heronarts.lx.LX;
 import heronarts.lx.Tempo;
 import heronarts.lx.Tempo.Quantization;
+import heronarts.lx.utils.LXUtils;
 
 public class QuantizedTriggerParameter extends TriggerParameter {
 
@@ -39,14 +42,40 @@ public class QuantizedTriggerParameter extends TriggerParameter {
 
   private OutputMode outputMode = OutputMode.SINGLE;
 
-  private static final List<QuantizedTriggerParameter> instances =
-    new ArrayList<QuantizedTriggerParameter>();
+  private static final Map<Tempo.Division, List<QuantizedTriggerParameter>> pendingInstances =
+    new HashMap<Tempo.Division, List<QuantizedTriggerParameter>>();
 
-  private final LXParameterListener quantizationListener = p -> {
+  static {
+    // Initialize pending array for all valid tempo divisions
+    for (Tempo.Division division : Tempo.Division.values()) {
+      pendingInstances.put(division, new ArrayList<QuantizedTriggerParameter>());
+    }
+  }
+
+  private final LXParameterListener quantizationListener = this::_onQuantizationChanged;
+
+  private void _onQuantizationChanged(LXParameter p) {
     if (getQuantization() == Tempo.Quantization.NONE) {
       resolve();
+    } else if (this.pending.isOn()) {
+      // The quantization value has changed while the parameter is pending,
+      _addPendingInstance();
     }
   };
+
+  private void _addPendingInstance() {
+    Tempo.Division division = this.quantization.getObject().getDivision();
+    if (division != null) {
+      // Register it in the pendingInstances array if it is not already there, avoid weird
+      // case where pending division goes from A->B->A all while it hasn't yet fired, so as
+      // to avoid duplicate instance in pending[A]. We are basically enforcing Set functionality
+      // on the cheaper small array storage
+      List<QuantizedTriggerParameter> pending = pendingInstances.get(division);
+      if (!pending.contains(this)) {
+        pending.add(this);
+      }
+    }
+  }
 
   public static enum OutputMode {
     /**
@@ -91,7 +120,6 @@ public class QuantizedTriggerParameter extends TriggerParameter {
     this.out = new TriggerParameter(label).onTrigger(onTrigger);
     this.pending = new BooleanParameter(label + "Pending").setMode(BooleanParameter.Mode.MOMENTARY);
     setQuantization(quantization);
-    instances.add(this);
   }
 
   public QuantizedTriggerParameter setQuantization(ObjectParameter<Tempo.Quantization> quantization) {
@@ -103,7 +131,7 @@ public class QuantizedTriggerParameter extends TriggerParameter {
       resolve();
     }
     if (this.quantization != null) {
-      this.quantization.addListener(this.quantizationListener);
+      this.quantization.addListener(this.quantizationListener, true);
     }
     return this;
   }
@@ -123,6 +151,9 @@ public class QuantizedTriggerParameter extends TriggerParameter {
 
   public QuantizedTriggerParameter setOutputMode(OutputMode outputMode) {
     this.outputMode = outputMode;
+    if (this.outputMode == OutputMode.SINGLE) {
+      this.semaphore = LXUtils.min(1, this.semaphore);
+    }
     return this;
   }
 
@@ -145,13 +176,21 @@ public class QuantizedTriggerParameter extends TriggerParameter {
   @Override
   protected void _onTrigger() {
     increment();
-
     Quantization quantization = getQuantization();
     if (!quantization.hasDivision() || quantization.getDivision().isActive()) {
       resolve();
     } else {
       this.pending.setValue(true);
+      _addPendingInstance();
     }
+  }
+
+  /**
+   * Cancels the pending state of this trigger
+   */
+  public void cancel() {
+    this.semaphore = 0;
+    this.pending.setValue(false);
   }
 
   /**
@@ -172,15 +211,36 @@ public class QuantizedTriggerParameter extends TriggerParameter {
 
   @Override
   public void dispose() {
-    instances.remove(this);
+    cancel();
     setQuantization(null);
     this.pending.dispose();
     this.out.dispose();
     super.dispose();
   }
 
+  /**
+   * Invoked from the Tempo engine to resolve all pending parameters when a
+   * particular launch quantization is fired. Should not be invoked manually.
+   *
+   * @param lx The LX instance
+   * @param division Tempo division to resolve
+   */
   public static void resolve(LX lx, Tempo.Division division) {
-    for (QuantizedTriggerParameter trigger : instances) {
+    final List<QuantizedTriggerParameter> instances = pendingInstances.get(division);
+    if (instances.isEmpty()) {
+      return;
+    }
+
+    // Make a copy of the instances we need to trigger - we do this because it is in theory
+    // possible for the resolution to create a chain of modulation/trigger mappings that
+    // could end up modifying the pendingInstances array. So we copy what we've got first
+    // and clear it to avoid concurrent array modification exceptions
+    QuantizedTriggerParameter[] copy = instances.toArray(new QuantizedTriggerParameter[0]);
+    instances.clear();
+
+    for (QuantizedTriggerParameter trigger : copy) {
+      // Check that the criteria are still met... pending state or quantization may have changed
+      // and we just let the cleanup happen lazily here by ignoring those that no longer match
       if (trigger.lx == lx &&
           trigger.pending.isOn() &&
           trigger.getQuantization().getDivision() == division) {
