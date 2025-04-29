5b5843041eb2ae4bbc331099c6d7d0d1f09f8775
Mark Slee <mcslee@gmail.com>
2025-04-29 09:48:55 +0100

    Bump to 1.1.1-SNAPSHOT

--- src/main/java/heronarts/lx/LX.java
+++ src/main/java/heronarts/lx/LX.java
@@ -70,7 +70,7 @@ import com.google.gson.stream.JsonWriter;
  */
 public class LX {
 
-  public static final String VERSION = "1.1.0";
+  public static final String VERSION = "1.1.1-SNAPSHOT";
 
   public static class InstantiationException extends Exception {
 
