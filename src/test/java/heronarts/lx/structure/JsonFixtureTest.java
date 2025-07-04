dbf9a16e16b011eeace43d545f76b205db310a17
Andrew Look <alook@makenotion.com>
2025-07-03 21:01:37 -0400

    basic test setup

---
+++ src/test/java/heronarts/lx/structure/JsonFixtureTest.java
@@ -0,0 +1,19 @@
+package heronarts.lx.structure;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JsonFixtureTest {
+    // @BeforeEach
+    // void setUp() {
+    //     evaluator = new StringExpressionEvaluator();
+    // }
+
+    // // Helper methods for cleaner test code
+    // private float evaluateNumeric(String expression) {
+    //     return evaluator.evaluateNumeric(expression);
+    // }
+}
