e48525c13cf07142cd210063cfa26c46801c45dd
Mark Slee <mcslee@gmail.com>
2026-08-03 12:05:39 -0500

    Cut 1.2.2 release

--- src/main/java/heronarts/lx/LX.java
+++ src/main/java/heronarts/lx/LX.java
@@ -70,7 +70,7 @@ import com.google.gson.stream.JsonWriter;
  */
 public class LX {
 
-  public static final String VERSION = "1.2.2-SNAPSHOT";
+  public static final String VERSION = "1.2.2";
 
   public static class InstantiationException extends Exception {
 
